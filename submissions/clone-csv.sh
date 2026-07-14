c16e3d275cea9d04176a59ec37e02bc5ddecd0fc
Filip Riha <riha.fina@gmail.com>
2026-07-14 15:01:46 +0200

    2025

--- submissions/clone-csv.sh
+++ submissions/clone-csv.sh
@@ -26,7 +26,8 @@ GIT_URL_COLUMN="Git clone URL"
 COMMIT_COLUMN="The whole commit hash we should consider"
 
 cat "$INPUT_FILE" \
-  | mlr --csv --headerless-csv-output --ofs=comma --ifs=$SEP cut -f "$GIT_URL_COLUMN,$COMMIT_COLUMN" \
+  | mlr --csv --ofs=comma --ifs=$SEP filter '($My == "y")' \
+  | mlr --csv --headerless-csv-output --ofs=comma --ifs=comma cut -f "$GIT_URL_COLUMN,$COMMIT_COLUMN" \
   | while IFS=',' read -r REPOSITORY COMMIT; do
     "$SCRIPTPATH/clone.sh" "$REPOSITORY" "$COMMIT" "$BASEDIR"
   done
